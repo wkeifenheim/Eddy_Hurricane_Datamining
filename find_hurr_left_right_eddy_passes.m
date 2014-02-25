75591ec398171a1cc533cf307a3015a4d1f59197
Woodrow Keifenheim <woodrowk@gmail.com>
2014-02-25 15:34:42 -0600

    verified code for determining distribution of left/center/right hurricane passing of eddies

--- find_hurr_left_right_eddy_passes.m
+++ find_hurr_left_right_eddy_passes.m
@@ -6,27 +6,40 @@
 % NaN - other..
 %
 
-axesm('pcarre'); %required for mfwdtran to work
+% axesm('pcarre'); %required for mfwdtran to work
 
-wait_h = waitbar(0,'determining left/right passes..');
+%wait_h = waitbar(0,'determining left/right passes..');
 arclen = 2;
 results = zeros(size(IBTrACS_1992_2010,1),1);
+dists = zeros(size(IBTrACS_1992_2010,1),1);
 
-for i = 1 : size(IBTrACS_1992_2010,1)
+parfor i = 1 : size(IBTrACS_1992_2010,1)
     
    if(~isnan(IBTrACS_1992_2010.EddyClass(i)))
-      [x1, y1] = mfwdtran(IBTrACS_1992_2010.Latitude_for_mapping(i),...
-          IBTrACS_1992_2010.Longitude_for_mapping(i));
-      [a, b] = reckon(IBTrACS_1992_2010.Latitude_for_mapping(i),...
-          IBTrACS_1992_2010.Longitude_for_mapping(i), arclen,...
-          IBTrACS_1992_2010.Azimuth(i));
-      [x2, y2] = mfwdtran(a,b);
-      [eddy_x, eddy_y] = mfwdtran(IBTrACS_1992_2010.EddyLat(i),...
-          IBTrACS_1992_2010.EddyLon(i));
-      results(i) = passes_right(x1, y1, x2, y2, eddy_x, eddy_y);
+%       [x1, y1] = mfwdtran(IBTrACS_1992_2010.Latitude_for_mapping(i),...
+%           IBTrACS_1992_2010.Longitude_for_mapping(i));
+      x1 = IBTrACS_1992_2010.Latitude_for_mapping(i);
+      y1 = IBTrACS_1992_2010.Longitude_for_mapping(i);
+      if(strcmp(IBTrACS_1992_2010.Serial_Num(i), IBTrACS_1992_2010.Serial_Num(i+1)));
+          x2 = IBTrACS_1992_2010.Latitude_for_mapping(i+1);
+          y2 = IBTrACS_1992_2010.Longitude_for_mapping(i+1);
+      else
+          x2 = NaN;
+          y2 = NaN;
+      end
+%       [x2, y2] = reckon(IBTrACS_1992_2010.Latitude_for_mapping(i),...
+%           IBTrACS_1992_2010.Longitude_for_mapping(i), arclen,...
+%           IBTrACS_1992_2010.Azimuth(i));
+%       [x2, y2] = mfwdtran(a,b);
+%       [eddy_x, eddy_y] = mfwdtran(IBTrACS_1992_2010.EddyLat(i),...
+%           IBTrACS_1992_2010.EddyLon(i));
+      eddy_x = IBTrACS_1992_2010.EddyLat(i);
+      eddy_y = IBTrACS_1992_2010.EddyLon(i);
+      [results(i), dists(i)] = passes_right(x1, y1, x2, y2, eddy_x, eddy_y);
    else
        results(i) = NaN;
+       dists(i) = NaN;
    end
-   waitbar(i/size(IBTrACS_1992_2010,1)) 
+   %waitbar(i/size(IBTrACS_1992_2010,1))
 end
-delete(wait_h);
+%delete(wait_h);
--- passes_right.m
+++ passes_right.m
@@ -1,20 +1,48 @@
 %Determine if hurricane passes to the left or right of an eddy center
 %Developed with reference to:
 %http://stackoverflow.com/questions/3461453/determine-which-side-of-a-line-a-point-lies
+%http://www.mathworks.com/matlabcentral/newsreader/view_thread/164048
 % on_left will be:
 % 1 - if true
 % 0 - if hurricane passes over the center of the eddy
 % -1 - if passes on the left
 %
+% Values for x/y distances from pcarre projection....
+% d = 0.0078 (50km)
+% d = 0.0039 (25km)
+%
+% Arclength:
+% d = 0.2248 (25km)
+% d = 0.4497 (50km)
 
-function on_right = passes_right(x1, y1, x2, y2, eddy_x, eddy_y)
-    
-    %mfwdtran requires a map projection to be in use...
-%     [p1.x, p1.y] = mfwdtran(x1, y1);
-%     [p2.x, p2.y] = mfwdtran(x2, y2);
-%     [e.x, e.y] = mfwdtran(eddy_x, eddy_y);
+function [on_right, dist] = passes_right(x1, y1, x2, y2, eddy_x, eddy_y)
     
     test_result = (x2-x1)*(eddy_y - y1) - (y2-y1)*(eddy_x-x1);
+    
+%     Q1 = [x1 y1];
+%     Q2 = [x2 y2];
+%     P = [eddy_x eddy_y];
+%     d = abs(det([Q2-Q1;P-Q1]))/norm(Q2-Q1);
+
+      % Using x/y coordinates....
+%     d_Q1_Q2_sqr = (x2-x1)^2 + (y2-y1)^2; %distance^2 from Q1 to Q2
+%     d_Q1_eddy_sqr =(eddy_x-x1)^2 + (eddy_y-y1)^2; %d^2 from Q1 to eddy
+%     d_Q2_eddy_sqr = d_Q1_eddy_sqr - d_Q1_Q2_sqr;
+%     d = sqrt(d_Q2_eddy_sqr);
+    
+%     [d_Q1_Q2, az_Q1_Q2] = distance(x1,y1,x2,y2);
+%     [d_Q1_eddy, az_Q1_eddy] = distance(x1,y1,eddy_x,eddy_y);
+%     theta = abs(az_Q1_Q2 - az_Q1_eddy);
+%     d = sind(theta) * d_Q1_eddy;
+%     dist = d;
+    dist = NaN;
+% 
+%     if(d < 0)
+%         disp('negative distance!...')
+%     end
+    
+%     if(d <= 0.4497) %trajectory within 25km of eddy center
+%         on_right = 0;
     if(test_result > 0)
         on_right = 1;
     elseif(test_result < 0)
@@ -22,9 +50,8 @@ function on_right = passes_right(x1, y1, x2, y2, eddy_x, eddy_y)
     elseif(test_result == 0)
         on_right = 0;
     else
-        %errordlg('on_left: something ain`t right..');
+        %errordlg('on_right: something ain`t right..');
         on_right = NaN;
     end
-
 
 end
