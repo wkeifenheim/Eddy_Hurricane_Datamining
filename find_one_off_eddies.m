9a09634d76dbcf86acf2a21e66ac8ec02cd32f48
Woodrow Keifenheim <woodrowk@gmail.com>
2014-03-20 12:38:28 -0500

    no comment

---
+++ find_one_off_eddies.m
@@ -0,0 +1,34 @@
+%determine indices of IBTrACS-eddies in which a hurricane interacts with an
+%eddy only once.
+
+cur_hur = IBTrACS_1992_2010.Serial_Num(1);
+one_offs = false(size(IBTrACS_1992_2010,1),1);
+e_class= NaN;
+e_idx = NaN;
+
+for i = 1 : length(one_offs)
+    
+    if(~strcmp(cur_hur, IBTrACS_1992_2010.Serial_Num(i)))
+        cur_hur = IBTrACS_1992_2010.Serial_Num(i);
+        e_class= NaN;
+        e_idx = NaN;
+    end
+    
+    if(~isnan(IBTrACS_1992_2010.EddyClass(i)))
+        if(e_class ~= IBTrACS_1992_2010.EddyClass(i) &&...
+                e_idx ~= IBTrACS_1992_2010.EddyIdx(i))
+            
+            e_idx = IBTrACS_1992_2010.EddyIdx(i);
+            e_class = IBTrACS_1992_2010.EddyClass(i);
+            if(e_class ~= IBTrACS_1992_2010.EddyClass(i+1) &&...
+                e_idx ~= IBTrACS_1992_2010.EddyIdx(i+1))
+                
+                one_offs(i) = true;
+            end
+        end
+    else
+        e_class = NaN;
+        e_idx = NaN;
+    end
+    
+end
--- plot_az_changes_3_groups.m
+++ plot_az_changes_3_groups.m
@@ -19,14 +19,15 @@ bins = -187.5:5:187.5;
 %bins = [-200, bins, 200];
 
 f = figure;
-set(f,'Name','Translational speeds in 50-75th percentile');
+set(f,'Name','Translational speeds in 25-50th percentile');
 
 all_eddies = IBTrACS_1992_2010(~isnan(IBTrACS_1992_2010.EddyClass(:)),:);
-all_eddies = all_eddies(bitand(all_eddies.Displacement_d1(:) >= disp_50,...
-    all_eddies.Displacement_d1(:) < disp_75),:);
+all_eddies = all_eddies(bitand(all_eddies.Displacement_d1(:) >= disp_25,...
+    all_eddies.Displacement_d1(:) < disp_50),:);
+% all_eddies = all_eddies(all_eddies.Displacement_d1(:) < disp_25,:);
 cyc_idx = all_eddies.EddyClass(:) == -1;
 acyc_idx= all_eddies.EddyClass(:) == 1;
-subplot(4,1,1);
+subplot(5,1,1);
 c1 = histc(all_eddies.Az_d1(cyc_idx),bins);
 a1 = histc(all_eddies.Az_d1(acyc_idx),bins);
 tots = [c1 a1];
@@ -35,11 +36,12 @@ title('Disribution of azimuth changes for all eddy interactions');
 ylabel('count');
 
 bound_eddies = IBTrACS_1992_2010(bound_idx,:);
-bound_eddies = bound_eddies(bitand(bound_eddies.Displacement_d1(:) >= disp_50,...
-    bound_eddies.Displacement_d1(:) < disp_75),:);
+bound_eddies = bound_eddies(bitand(bound_eddies.Displacement_d1(:) >= disp_25,...
+    bound_eddies.Displacement_d1(:) < disp_50),:);
+% bound_eddies = bound_eddies(bound_eddies.Displacement_d1(:) < disp_25,:);
 cyc_idx2 = bound_eddies.EddyClass(:) == -1;
 acyc_idx2 = bound_eddies.EddyClass(:) == 1;
-subplot(4,1,2);
+subplot(5,1,2);
 c1 = histc(bound_eddies.Az_d1(cyc_idx2),bins);
 a1 = histc(bound_eddies.Az_d1(acyc_idx2),bins);
 tots = [c1 a1];
@@ -48,11 +50,12 @@ title('Disribution of azimuth changes for bound eddy interactions (25-50km)');
 ylabel('count');
 
 outside_eddies = IBTrACS_1992_2010(outside_idx,:);
-outside_eddies = outside_eddies(bitand(outside_eddies.Displacement_d1(:) >= disp_50,...
-    outside_eddies.Displacement_d1(:) < disp_75),:);
+outside_eddies = outside_eddies(bitand(outside_eddies.Displacement_d1(:) >= disp_25,...
+    outside_eddies.Displacement_d1(:) < disp_50),:);
+% outside_eddies = outside_eddies(outside_eddies.Displacement_d1(:) < disp_25,:);
 cyc_idx3 = outside_eddies.EddyClass(:) == -1;
 acyc_idx3 = outside_eddies.EddyClass(:) == 1;
-subplot(4,1,3);
+subplot(5,1,3);
 c1 = histc(outside_eddies.Az_d1(cyc_idx3),bins);
 a1 = histc(outside_eddies.Az_d1(acyc_idx3),bins);
 tots = [c1 a1];
@@ -62,15 +65,33 @@ ylabel('count');
 xlabel('change of azimuth')
 
 inside_eddies = IBTrACS_1992_2010(inside_idx,:);
-inside_eddies = inside_eddies(bitand(inside_eddies.Displacement_d1(:) >= disp_50,...
-    inside_eddies.Displacement_d1(:) < disp_75),:);
+inside_eddies = inside_eddies(bitand(inside_eddies.Displacement_d1(:) >= disp_25,...
+    inside_eddies.Displacement_d1(:) < disp_50),:);
+% inside_eddies = inside_eddies(inside_eddies.Displacement_d1(:) < disp_25,:);
+inside_eddies = inside_eddies(inside_eddies.Latitude_for_mapping(:) > 0,:);
 cyc_idx3 = inside_eddies.EddyClass(:) == -1;
 acyc_idx3 = inside_eddies.EddyClass(:) == 1;
-subplot(4,1,4);
+subplot(5,1,4);
 c1 = histc(inside_eddies.Az_d1(cyc_idx3),bins);
 a1 = histc(inside_eddies.Az_d1(acyc_idx3),bins);
 tots = [c1 a1];
 bar(bins,tots,'histc')
-title('Disribution of azimuth changes of eddy interactions inside bound (<=25km)');
+title('Disribution of North hemisphere azimuth changes of eddy interactions inside bound (<=25km)');
+ylabel('count');
+xlabel('change of azimuth')
+
+inside_eddies = IBTrACS_1992_2010(inside_idx,:);
+inside_eddies = inside_eddies(bitand(inside_eddies.Displacement_d1(:) >= disp_25,...
+    inside_eddies.Displacement_d1(:) < disp_50),:);
+% inside_eddies = inside_eddies(inside_eddies.Displacement_d1(:) < disp_25,:);
+inside_eddies = inside_eddies(inside_eddies.Latitude_for_mapping(:) < 0,:);
+cyc_idx3 = inside_eddies.EddyClass(:) == -1;
+acyc_idx3 = inside_eddies.EddyClass(:) == 1;
+subplot(5,1,5);
+c1 = histc(inside_eddies.Az_d1(cyc_idx3),bins);
+a1 = histc(inside_eddies.Az_d1(acyc_idx3),bins);
+tots = [c1 a1];
+bar(bins,tots,'histc')
+title('Disribution of South hemisphere azimuth changes of eddy interactions inside bound (<=25km)');
 ylabel('count');
 xlabel('change of azimuth')
